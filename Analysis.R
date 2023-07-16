9b6296b2effd1d61ff3b8b8408b8303b1383026f
PALMER SWANSON <pjswanson@fsu.edu>
2023-07-16 12:20:16 -0400

    named inner and outer lists.  Can now extract each condition and turn into df for tables/plotting

--- Analysis.R
+++ Analysis.R
@@ -1,5 +1,5 @@
 source("functions.R")
-
+library(stats) #For setnames function
 #Applies to the test case
 # test1 <- gamma_study(test)
 # test2 <- y_gen_study(test)
@@ -12,16 +12,26 @@ source("functions.R")
 # row.names(check) <- sample_size
 # df <- melt(as.matrix(check))
 
-#Need to use lapply
+#Need to use lapply across these rows
 ah <- lapply(simulation, gamma_study)
 ah1 <- lapply(ah, gamm_interval_construct)
 ah2 <- lapply(ah1, inlc_excl_gam)
 ah3 <- lapply(ah2, sum_inlc_excl)
 ah4 <- lapply(ah3, percent_in_ex)
 
-#name the output list to match up with the conditions of the simulation
-names <- apply(sim_conditions, MARGIN = 1, function(x) paste0(x[1], " condition:", x[2]))
-names(ah4) <- names 
+
+
+#Naming lists.  Outer list
+outer_names <- apply(sim_conditions, MARGIN = 1, function(x) paste0(x[1], " condition:", x[2]))
+#Naming inner list
+inner_names <- apply(conditions, MARGIN = 1, function(x) paste0("n= ", x[1], "p= ", x[2], "SE= ", x[3]))
+
+#setnames, then lapply(nested list, call setNames, call the inner names), then finally outer names
+named_list <- setNames(lapply(ah4, setNames, inner_names), outer_names)
+
+#Saving this output
+#Saving output of simulation to enviroment
+saveRDS(named_list, file = "small simulation results")
 
 
 #Plotting probability of inclusion.
@@ -30,7 +40,6 @@ p <- ggplot(df, aes(x=X1, y=value, color = X2)) + geom_point() +
   scale_color_discrete(labels = paste("Gamma", 1:10)) + 
   labs(x = "Sample Size", y = "Probability of inclusion", title = "Probability of Variable Inclusion vs. Sample Size")
 p
-
 
 #Tables?
 library(gt)
